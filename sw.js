b9c611bdd43bbdb5b80b8cc7dde4ab6851e0f7e6
DAS Latam <daslatamarg@gmail.com>
2026-01-02 20:56:07 +0000

    BathCalc

---
+++ sw.js
@@ -0,0 +1,22 @@
+// Actualizamos a v2 para que el navegador sepa que hay cambios
+const CACHE_NAME = 'baht-calc-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting(); // Fuerza la actualización inmediata
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
